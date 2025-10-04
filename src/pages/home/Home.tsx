57ca1158176a0833373908204b227da327d73135
Hazmatt101 <matt.mccortney@gmail.com>
2025-10-04 13:18:42 -0700

    Home page work

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/shared/header/Header"
 import "./Home.scss";
 
+
 export const Home = () => {
     return (
         <>
@@ -9,7 +10,7 @@ export const Home = () => {
                 <h2 className="text-xl font-semibold">Home</h2>
                 <div className="about-section">
                     <h3>About Me</h3>
-                    <p>My name is Matt McCortney and I am a senior fullstack software engineer. I have a deep passion \nfor creating quality web and desktop applications, and I have a user-obsessed mindset when it comes to UX/UI design.</p>
+                    <p>My name is Matt McCortney and I am a senior full-stack software engineer. I have a deep passion \nfor creating quality web and desktop applications, and I have a user-obsessed mindset when it comes to UX/UI design and development.</p>
                 </div>
             </div>
         </>
