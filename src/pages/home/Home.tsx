aa533734299d87c8244bc080a9b611546eae679a
MattMcCortney <matt.mccortney@gmail.com>
2025-10-04 14:52:56 -0700

    Finish draft of Header

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,30 @@
-import { Header } from "@/shared/header/Header"
+import { Header } from "@/shared/header/Header";
 import "./Home.scss";
 
-
 export const Home = () => {
-    return (
-        <>
-            <Header />
-            <div>
-                <h2 className="text-xl font-semibold">Home</h2>
-                <div className="about-section">
-                    <h3>About Me</h3>
-                    <p>My name is Matt McCortney and I am a senior full-stack software engineer. I have a deep passion \nfor creating quality web and desktop applications, and I have a user-obsessed mindset when it comes to UX/UI design and development.</p>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="page-title">
+        <h1 className="text-2xl font-bold">Matt McCortney</h1>
+        <h4 className="text-l">
+          Senior Full-stack/Frontend/Backend Software Engineer with over 6 years
+          of experience in developing and optimizing scalable software
+          solutions. Passionate about driving innovation and delivering
+          high-quality results.
+        </h4>
+      </div>
+      <div>
+        <div className="about-section">
+          <h3>About Me</h3>
+          <p>
+            My name is Matt McCortney and I am a senior full-stack software
+            engineer. I have a deep passion for creating quality web and
+            desktop applications, and I have a user-obsessed mindset when it
+            comes to UX/UI design and development.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
+import "./Header.scss";
 
 export const Header = () => {
-
-    return (
-        <header className="p-4 bg-gray-200 navy-blue">
-            <h1 className="text-2xl font-bold">Matt McCortney</h1>
-        </header>
-    )
-}
+  const links = [
+    { link: "/about", text: "About Me" },
+    { link: "/projects", text: "Projects" },
+    { link: "/volunteering", text: "Volunteering" },
+    { link: "/contact", text: "Contact Me" },
+  ];
+  return (
+    <header className="p-4 bg-gray-200 header">
+      <div className="header-controls">
+        {
+            links.flatMap((url) => {
+                return (
+                    <Link href={url.link}>{url.text}</Link>
+                )
+            })
+        }
+      </div>
+    </header>
+  );
+};
